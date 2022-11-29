c89b4f90dfce613ef9e1bd40024834912d70a247
Kevin Savo <kevinsavo@Kevins-MacBook-Pro.local>
2022-11-29 01:06:06 -0700

    upd

---
+++ script.js
@@ -0,0 +1,47 @@
+// Assignment Code
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+function getPasswordCriteria() {
+  var userChoseNumbers = confirm("Do you want numbers in your password?");
+  var options = {
+    userChoseNumbers: userChoseNumbers
+  };
+  return options;
+}
+
+function createRandom(length) {
+  var randomNum = Math.floor(Math.random() * length);
+  return randomNum;
+}
+
+function generatePassword() {
+    var userChose = getPasswordCriteria();
+    var availableChars = [];
+    var passwordArr = [];
+    if(userChose.userChoseNumbers) {
+      availableChars = availableChars.concat(numbers);
+    }
+
+    for(var i = 0; i < 4; i++) {
+      passwordArr.push(availableChars[createRandom(availableChars.length)])
+    }
+
+
+  return passwordArr.join("")
+}
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+  
