5600a014f7c2bf525857ebf1bc72af0d7cbb3412
Kevin Savo <kevinsavo@Kevins-MacBook-Pro.local>
2023-01-23 21:51:14 -0700

    updated

--- script.js
+++ script.js
@@ -1,47 +1,59 @@
 // Assignment Code
-var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
-function getPasswordCriteria() {
-  var userChoseNumbers = confirm("Do you want numbers in your password?");
-  var options = {
-    userChoseNumbers: userChoseNumbers
-  };
-  return options;
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var SpecialCharacters =['!', '@', '#', '$', '%', '^', '&', '*', '+', '?'];
+var lowerCase = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
+var upperCase = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"];
+function getpasswordscriteria(){
+var pass = prompt("how many characters would you like in your password?");
+var userChoseNumbers = confirm("would you like numbers in your password?");
+var userChoseSpecialCharacters = confirm("would you like special characters in your password?");
+var lowerCase = confirm("would you like lower case in your password?");
+var upperCase = confirm("would you like upper case in your password?");
+var options = {
+  userChoseNumbers: userChoseNumbers
 }
-
+return options;
+}
 function createRandom(length) {
-  var randomNum = Math.floor(Math.random() * length);
-  return randomNum;
+var randomNum = Math.floor(Math.random() * length);
+return randomNum;
 }
-
 function generatePassword() {
-    var userChose = getPasswordCriteria();
-    var availableChars = [];
-    var passwordArr = [];
-    if(userChose.userChoseNumbers) {
-      availableChars = availableChars.concat(numbers);
-    }
-
-    for(var i = 0; i < 4; i++) {
-      passwordArr.push(availableChars[createRandom(availableChars.length)])
-    }
-
-
-  return passwordArr.join("")
+  var userchose = getpasswordscriteria();
+  var availablechars = [];
+ var passwordArr = [];
+  if(userchose.userChoseNumbers){
+    availablechars = availablechars.concat(numbers).concat(SpecialCharacters).concat(lowerCase).concat(upperCase);
+  }
+  for(var i = 0; i < 128; i++){
+  passwordArr.push(availablechars[createRandom(availablechars.length)])
+  }
+   return passwordArr.join("")
 }
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-  
+white_check_mark
+eyes
+raised_hands
+
+
+
+
+
+
+
+
+
+
+
+
+
+
